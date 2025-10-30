735c1f5354d3004da1d0ed7b8cbd8778aba1734f
Joao <j.pereirinha@campus.fct.unl.pt>
2025-10-30 02:59:26 +0000

    -

--- proj1_homeway/dataStructures/exceptions/InvalidLocationException.java
+++ proj1_homeway/dataStructures/exceptions/InvalidLocationException.java
@@ -1,3 +1,7 @@
+/**
+* @author João Pereirinha 64382 j.pereirinha@campus.fct.unl.pt
+* @author Miguel Silva 68510 masa.silva@campus.fct.unl.pt
+*/
 package dataStructures.exceptions;
 
 public class InvalidLocationException extends Exception {
--- proj1_homeway/interfaces/EatingService.java
+++ proj1_homeway/interfaces/EatingService.java
@@ -1,3 +1,7 @@
+/**
+* @author João Pereirinha 64382 j.pereirinha@campus.fct.unl.pt
+* @author Miguel Silva 68510 masa.silva@campus.fct.unl.pt
+*/
 package interfaces;
 
 public interface EatingService extends Service {
